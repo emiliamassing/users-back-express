a89ceef7015737c778a7e0b78d1f6fa8d51efa72
emiliamassing <emiliamassings@gmail.com>
2023-03-03 15:22:40 +0100

    added router.post for login system

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,27 @@ const router = express.Router();
 
 
 let users = [
-  {username: 'Emilia', password: 'merlin'},
-  {username: 'Hayden', password: 'anakin'},
-  {username: 'Sid', password: 'wilson'}
+  {username: 'Emilia', password: 'merlin', id: 0},
+  {username: 'Hayden', password: 'anakin', id: 1},
+  {username: 'Sid', password: 'wilson', id: 2}
 ];
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Login');
+
+router.get('/', function(req, res) {
+  res.json(users);
+});
+
+router.post('/login', function(req, res, next) {
+  const {username, password} = req.body;
+  const foundUser = users.find(user => user.username === username);
+
+  if(password === foundUser.password) {
+    res.status(201).json({username: foundUser.username, id: foundUser.id});
+  } else {
+    res.status(401).json('Incorrect password or username');
+  }
+
 });
 
 module.exports = router;
