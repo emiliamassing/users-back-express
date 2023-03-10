bf07d91b7648db25c9d31edfb98a2298797f1779
emiliamassing <emiliamassings@gmail.com>
2023-03-10 11:38:19 +0100

    installed crypto-js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 let users = [
   {username: 'Emilia', password: 'merlin', id: 0},
   {username: 'Hayden', password: 'anakin', id: 1},
@@ -14,15 +13,20 @@ router.get('/', function(req, res) {
   res.json(users);
 });
 
-router.post('/login', function(req, res, next) {
+router.post('/login', function(req, res) {
   const { username, password } = req.body;
   const foundUser = users.find(user => user.username === username);
 
-  if(password === foundUser.password) {
-    res.status(201).json({name: foundUser.username, id: foundUser.id});
-  } else {
-    res.status(401).json('Incorrect password or username');
-  }
+  console.log(foundUser);
+
+  if(foundUser){
+    if(password === foundUser.password) {
+      res.status(201).json({name: foundUser.username, id: foundUser.id});
+    } else {
+      res.status(401).json('Incorrect password or username');
+    };
+  };
+  
 });
 
 module.exports = router;
